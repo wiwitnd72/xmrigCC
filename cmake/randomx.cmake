54a0bc4393dd3cf1bac3018b9b2bc673729ea01d
Ben Gräf <ben@graef.in>
2019-12-10 21:19:45 +0100

    More arm64 fixes

--- cmake/randomx.cmake
+++ cmake/randomx.cmake
@@ -51,6 +51,17 @@ if (WITH_RANDOMX)
             )
         # cheat because cmake and ccache hate each other
         set_property(SOURCE src/crypto/randomx/jit_compiler_x86_static.S PROPERTY LANGUAGE C)
+    elseif (XMRIG_ARM AND CMAKE_SIZEOF_VOID_P EQUAL 8)
+        list(APPEND SOURCES_CRYPTO
+             src/crypto/randomx/jit_compiler_a64_static.S
+             src/crypto/randomx/jit_compiler_a64.cpp
+            )
+        # cheat because cmake and ccache hate each other
+        set_property(SOURCE src/crypto/randomx/jit_compiler_a64_static.S PROPERTY LANGUAGE C)
+    endif()
+
+    if (CMAKE_CXX_COMPILER_ID MATCHES Clang)
+        set_source_files_properties(src/crypto/randomx/jit_compiler_x86.cpp PROPERTIES COMPILE_FLAGS -Wno-unused-const-variable)
     endif()
 else()
     remove_definitions(/DXMRIG_ALGO_RANDOMX)
